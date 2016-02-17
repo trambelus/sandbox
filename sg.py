31c0c8da0ccd66d453f537a54e053b7560e6055f
Trambelus <trambelus2@gmail.com>
2016-02-17 15:02:18 -0500

    Removed lambda; changed to d8

--- sg.py
+++ sg.py
@@ -11,12 +11,12 @@ def combinations(iterable, r):
             yield tuple(pool[i] for i in indices)
 
 ops = [
-	[lambda x,y: x+y, "{0} + {1}"],
-	[lambda x,y: x-y, "{0} - {1}"],
-	[lambda x,y: y-x, "{1} - {0}"],
-	[lambda x,y: x*y, "{0} * {1}"],
-	[lambda x,y: x/y, "{0} / {1}"],
-	[lambda x,y: y/x, "{1} / {0}"],
+	[0, "{0} + {1}"],
+	[1, "{0} - {1}"],
+	[2, "{1} - {0}"],
+	[3, "{0} * {1}"],
+	[4, "{0} / {1}"],
+	[5, "{1} / {0}"],
 ]
 
 def primes(level, limit=3):
@@ -32,15 +32,24 @@ def primes(level, limit=3):
 	p3 = [primes[i:i+limit] for i in range(0, len(primes), limit)]
 	return p3[level-1]
 
-def process_multi(dice, targets, index, index_total, pc, quick, stopped):
-	ops = [
-		[lambda x,y: x+y, "{0} + {1}"],
-		[lambda x,y: x-y, "{0} - {1}"],
-		[lambda x,y: y-x, "{1} - {0}"],
-		[lambda x,y: x*y, "{0} * {1}"],
-		[lambda x,y: x/y, "{0} / {1}"],
-		[lambda x,y: y/x, "{1} / {0}"],
-	]
+qprimes = [[3, 5, 7], [11, 13, 17], [19, 23, 29], [31, 37, 41], [43, 47, 53], [59, 61, 67], [71, 73, 79], [83, 89, 97], [101, 103, 107]]
+
+def opf(i,x,y):
+	if i == 0:
+		return x+y
+	if i == 1:
+		return x-y
+	if i == 2:
+		return y-x
+	if i == 3:
+		return x*y
+	if i == 4:
+		return x/y
+	if i == 5:
+		return y/x
+	return None
+
+def process_multi(dice, targets, index, index_total, pc, quick):
 
 	dperms = list(permutations(dice))
 	operms = list(combinations(ops, len(dice)-1))
@@ -52,15 +61,15 @@ def process_multi(dice, targets, index, index_total, pc, quick, stopped):
 			if (idp*oplen+iop)%index_total != index:
 				continue
 
-			if stopped.value:
+			if quick and pc.value:
 				return
 
 			dp = dperms[idp]
 			op = operms[iop]
 			try:
-				num = op[0][0](dp[0],dp[1])
+				num = opf(op[0][0],dp[0],dp[1])
 				for i in range(2, len(dp)):
-					num = op[i-1][0](num,dp[i])
+					num = opf(op[i-1][0],num,dp[i])
 			except ZeroDivisionError:
 				continue
 			if num in targets:
@@ -73,11 +82,11 @@ def process_multi(dice, targets, index, index_total, pc, quick, stopped):
 
 def process(level, dice, quick=False):
 
-	targets = primes(level)
-	dperms = list(permutations(dice))
-	operms = list(combinations(ops, len(dice)-1))
+	targets = qprimes[level-1]
 
 	if not quick:
+		dperms = list(permutations(dice))
+		operms = list(combinations(ops, len(dice)-1))
 		print("len(operms) = {}".format(len(operms)))
 		print()
 		print("len(dperms) = {}".format(len(dperms)))
@@ -87,20 +96,20 @@ def process(level, dice, quick=False):
 
 	pc = mp.Value('i')
 	pc.value = 0
-	stopped = mp.Value('b')
-	stopped.value = 0
 
 	num_proc = mp.cpu_count()
 	processes = []
 	for i in range(num_proc):
-		processes.append(mp.Process(target=process_multi, args=(dice, targets, i, num_proc, pc, quick, stopped)))
+		processes.append(mp.Process(target=process_multi, args=(dice, targets, i, num_proc, pc, quick)))
 		processes[-1].start()
 
 	while any([process.is_alive() for process in processes]):
 		if not quick:
 			time.sleep(1)
-		if quick and pc.value != 0:
-			stopped.value = 1
+		else:
+			time.sleep(0.1)
+		if quick and pc.value:
+			break
 
 	if not quick:
 		print("\nWorking combinations: {0}".format(pc.value))
--- sgm.py
+++ sgm.py
@@ -6,29 +6,30 @@ from sg import process
 import sqlite3
 from pprint import pprint
 
-MIN_DICE = 2
+MIN_DICE = 5
 MAX_DICE = 8
 
-MAX_DIE = 6
+MAX_DIE = 8
 
 def wait(stop):
-	if input() == 'q':
+	#if input() == 'q':
 		stop[0] = True
 
 def init_db():
 	db = sqlite3.connect('sgm.db3')
-	db.execute("""CREATE TABLE IF NOT EXISTS d6 (
+	db.execute("""CREATE TABLE IF NOT EXISTS d8 (
 		id TEXT PRIMARY KEY UNIQUE,
 		result DOUBLE,
 		num_tests INT
 	);""")
-	return db
+	db.commit()
+	db.close()
 
 def save(results):
 	db = sqlite3.connect('sgm.db3')
 	for level in range(len(results)):
 		for num_dice in range(len(results[0])):
-			db.execute("REPLACE INTO d6 (id, result, num_tests) VALUES (?,?,?)",
+			db.execute("REPLACE INTO d8 (id, result, num_tests) VALUES (?,?,?)",
 				("{};{}".format(level+1, num_dice+MIN_DICE), results[level][num_dice][0], results[level][num_dice][1]))
 
 	db.commit()
@@ -55,8 +56,8 @@ def main():
 				res = process(level, dice, quick=True)
 
 				#print(results)
-				current = results[level-1][num_dice-2][0]
-				num_tests = results[level-1][num_dice-2][1]
+				current = results[level-1][num_dice-MIN_DICE][0]
+				num_tests = results[level-1][num_dice-MIN_DICE][1]
 
 				if num_tests == 0:
 					results[level-1][num_dice-MIN_DICE] = [100*float(res), 1]
@@ -64,7 +65,7 @@ def main():
 					results[level-1][num_dice-MIN_DICE][0] = (num_tests*current + 100*float(res)) / (num_tests + 1)
 					results[level-1][num_dice-MIN_DICE][1] = num_tests + 1
 
-		#pprint(results)
+		pprint(results)
 		save(results)
 
 if __name__ == '__main__':
